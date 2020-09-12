9ff0f3c340b802991699f2f3132ef1b641309f20
ogawatti <ogawattim@gmail.com>
2020-09-12 22:40:25 +0900

    replace index content to sample image

---
+++ components/SampleImg.vue
@@ -0,0 +1,8 @@
+<template>
+  <img class="SampleImg" src="@/assets/sample.jpg" width="800"/>
+</template>
+<style>
+.SampleImg {
+  margin: auto;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,39 +1,19 @@
 <template>
   <div class="container">
     <div>
-      <logo />
-      <h1 class="title">
-        nuxt-sample
-      </h1>
-      <h2 class="subtitle">
-        My perfect Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+      <h1 class="title">Sample Image</h1>
+      <h4 class="subtitle">6016x3760 (4.2MB)</h4>
+      <sample-img />
     </div>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import SampleImg from '~/components/SampleImg.vue'
 
 export default {
   components: {
-    Logo
+    SampleImg
   }
 }
 </script>
@@ -64,9 +44,5 @@ export default {
   color: #526488;
   word-spacing: 5px;
   padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
 }
 </style>
