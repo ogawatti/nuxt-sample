cfdbc6e3a386f000767b78a5c67c1f622abeaeec
ogawatti <ogawattim@gmail.com>
2020-11-05 13:13:26 +0900

    add form page

---
+++ pages/form.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="container">
+    <div>
+      <h1 class="title">Sample Form</h1>
+      <form name="contact" method="POST" netlify>
+        <p>
+          <label>Your Name: <input type="text" name="name" /></label>   
+        </p>
+        <p>
+          <label>Your Email: <input type="email" name="email" /></label>
+        </p>
+        <p>
+          <label>Subject: <input type="text" name="subject" /></label>   
+        </p>
+        <p>
+          <label>Message: <textarea name="message"></textarea></label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+import SampleImg from '~/components/SampleImg.vue'
+
+export default {
+  components: {
+    SampleImg
+  },
+  data () {
+    return {
+      variable: process.env.VARIABLE
+    }
+  }
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+</style>
+
